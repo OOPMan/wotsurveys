1076db85985f56bbc499e13ba1d9cde2f3591440
Adam Jorgensen <adamj@sjsoft.com>
2013-05-06 11:47:13 +0200

    Simplified WoTSurveys Servlet

--- src/main/scala/com/wot/wotsurveys/WoTSurveys.scala
+++ src/main/scala/com/wot/wotsurveys/WoTSurveys.scala
@@ -1,17 +1,9 @@
 package com.wot.wotsurveys
 
-import org.scalatra._
-import scalate.ScalateSupport
-
 class WoTSurveys extends WotSurveysStack {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    contentType = "text/html"
+    layoutTemplate("/WEB-INF/views/default.jade")
   }
-  
 }
