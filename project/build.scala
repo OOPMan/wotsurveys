947133ab7f4b012ff465e5cb3d205fd35a51f9bf
Adam Jorgensen <adamj@sjsoft.com>
2013-05-06 15:47:02 +0200

    Added license Updated dependencies

--- project/build.scala
+++ project/build.scala
@@ -1,3 +1,14 @@
+
+/**
+ * This file is part of WoTSurveys
+ *
+ * It is subject to the license terms in the LICENSE.txt file found in the top-level directory of this distribution and
+ * at http://eclipse.org/org/documents/epl-v10.php.
+ *
+ * No part of WoTSurveys, including this file, may be copied, modified, propagated, or distributed except according
+ * to the terms contained in the LICENSE file.
+ */
+
 import sbt._
 import Keys._
 import org.scalatra.sbt._
@@ -9,39 +20,43 @@ object WotSurveysBuild extends Build {
   val Organization = "com.wot"
   val Name = "WoT Surveys"
   val Version = "0.1.0-SNAPSHOT"
-  val ScalaVersion = "2.10.0"
-  val ScalatraVersion = "2.2.0"
+  val ScalaVersion = "2.10.1"
+  val ScalatraVersion = "2.2.1"
 
-  lazy val project = Project (
+  lazy val project = Project(
     "wot-surveys",
     file("."),
     settings = Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ com.typesafe.startscript.StartScriptPlugin.startScriptForClassesSettings ++
-    Seq(
-      organization := Organization,
-      name := Name,
-      version := Version,
-      scalaVersion := ScalaVersion,
-      resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
-      libraryDependencies ++= Seq(
-        "com.typesafe.slick" %% "slick" % "1.0.0",
-        "postgresql" % "postgresql" % "9.1-901.jdbc4",
-        "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "compile;container",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "compile;container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
-      ),
-      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
-        Seq(
-          TemplateConfig(
-            base / "webapp" / "WEB-INF" / "templates",
-            Seq.empty,  /* default imports should be added here */
-            Seq.empty,  /* add extra bindings here */
-            Some("templates")
-          )
-        )
-      }
-    )
+      Seq(
+        organization := Organization,
+        name := Name,
+        version := Version,
+        scalaVersion := ScalaVersion,
+        resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+        libraryDependencies ++= Seq(
+          "com.typesafe.slick" %% "slick" % "1.0.0",
+          "postgresql" % "postgresql" % "9.1-901.jdbc4",
+          "c3p0" % "c3p0" % "0.9.1.2",
+          "org.scalatra" %% "scalatra" % ScalatraVersion,
+          "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+          "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+          "org.scalatra" %% "scalatra-json" % ScalatraVersion,
+          "org.json4s"   %% "json4s-jackson" % "3.1.0",
+          "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
+          "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "compile;container",
+          "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "compile;container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+        ),
+        scalateTemplateConfig in Compile <<= (sourceDirectory in Compile) {
+          base =>
+            Seq(
+              TemplateConfig(
+                base / "webapp" / "WEB-INF" / "templates",
+                Seq.empty, /* default imports should be added here */
+                Seq.empty, /* add extra bindings here */
+                Some("templates")
+              )
+            )
+        }
+      )
   )
 }
