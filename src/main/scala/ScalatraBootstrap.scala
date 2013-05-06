2a293f79e83bccc727bb1d0912b41d52656ff42d
Adam Jorgensen <adamj@sjsoft.com>
2013-05-06 12:34:09 +0200

    Added EPL Mount Surveys servlet

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -2,8 +2,19 @@ import com.wot.wotsurveys._
 import org.scalatra._
 import javax.servlet.ServletContext
 
+/**
+ * This file is part of WoTSurveys
+ *
+ * It is subject to the license terms in the LICENSE.txt file found in the top-level directory of this distribution and
+ * at http://eclipse.org/org/documents/epl-v10.php.
+ *
+ * No part of WoTSurveys, including this file, may be copied, modified, propagated, or distributed except according
+ * to the terms contained in the LICENSE file.
+ */
+
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
+    context.mount(new Surveys, "/surveys/*")
     context.mount(new WoTSurveys, "/*")
   }
 }
